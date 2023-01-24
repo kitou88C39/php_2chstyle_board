2a313d4db89674485f9bf2e317912e232a963614
kitou88C39 <kanfutrooper@icloud.com>
2023-01-24 16:26:30 +0900

    database接続

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ if(!empty ($_POST["submitButton"])){
 echo $_POST["username"];
 echo $_POST["comment"];
 }
+//DB接続
+$pdo = new PDO('mysql:host=localhost;dbname=test',$user,$pass);
 ?>
 
 <!DOCTYPE html>
